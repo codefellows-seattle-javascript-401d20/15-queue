797208489995071b6f617d61a2ebda48b631de91
KatherineHanson <katherinemariehanson@gmail.com>
2017-10-02 11:39:47 -0700

    moved all files and folders into lab-katherine folder

---
+++ lab-katherine/__test__/queue.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const queue = require('../model/queue.js');
+const getQueue = () => queue();
+
+describe('queue', () => {
+  describe('#enqueue and #dequeue', () => {
+    test('the queue should have a first in first out behavior', () => {
+      let queue = getQueue();
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      expect(queue.dequeue()).toEqual(1);
+      expect(queue.dequeue()).toEqual(2);
+      expect(queue.dequeue()).toEqual(3);
+      expect(queue.dequeue()).toEqual(undefined);
+    });
+  });
+});
