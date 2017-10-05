3384b0e27affa5a18ebdc72cc4f536fce0f0136c
Katherine Hanson <KatherineHanson@users.noreply.github.com>
2017-10-05 10:13:26 -0700

    added test for handling undefined

--- lab-katherine/__test__/queue.test.js
+++ lab-katherine/__test__/queue.test.js
@@ -15,5 +15,21 @@ describe('queue', () => {
       expect(queue.dequeue()).toEqual(3);
       expect(queue.dequeue()).toEqual(undefined);
     });
+
+    test('enqueueing undefined should dequeue undefined', () => {
+      let queue = getQueue();
+      queue.enqueue(undefined);
+      queue.enqueue(undefined);
+      queue.enqueue(undefined);
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      expect(queue.dequeue()).toEqual(undefined);
+      expect(queue.dequeue()).toEqual(undefined);
+      expect(queue.dequeue()).toEqual(undefined);
+      expect(queue.dequeue()).toEqual(1);
+      expect(queue.dequeue()).toEqual(2);
+      expect(queue.dequeue()).toEqual(3);
+    });
   });
 });
